0ab8900a7fd8797dba7db1235a15bd989883f968
Marisa Settimi <marisasettimi@Marisas-MBP.fc.ca>
2020-01-10 11:42:59 -0500

    added java

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// this is a self-invoking anonymous function
+// also called an IIFE
+// also called a module (its a pattern)
+
+(() => {
+// for ex. python = print('whatever');
+  console.log('hello from JS!');
+
+// find the elements in the document
+  let toggleButton = document.querySelector(".switch-text"),
+      myHeading = document.querySelector('h1'),
+      svgImg = document.querySelector("#badge");
+
+// this function should change the heading text
+  function changeText() {
+    myHeading.textContent = "What is up? I am script";
+  }
+
+// this function should log the ID for each SVG to the console
+  function logSVG() {
+    console.log(this.id);
+  }
+
+  toggleButton.addEventListener("click", changeText);
+  svgImg.addEventListener("mouseover", logSVG);
+
+// document.querySelector('h1').textContent = "What is up? I am script";
+})();
